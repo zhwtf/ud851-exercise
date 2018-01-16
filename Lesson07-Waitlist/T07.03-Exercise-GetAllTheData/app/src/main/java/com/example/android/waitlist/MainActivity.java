6cea9c2e2223869a7295705f26da8709659d87bc
haozheng <hza89@sfu.ca>
2018-01-16 13:44:38 -0800

    complete

--- Lesson07-Waitlist/T07.03-Exercise-GetAllTheData/app/src/main/java/com/example/android/waitlist/MainActivity.java
+++ Lesson07-Waitlist/T07.03-Exercise-GetAllTheData/app/src/main/java/com/example/android/waitlist/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.android.waitlist;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+
+import com.example.android.waitlist.data.TestUtil;
+import com.example.android.waitlist.data.WaitlistContract;
+import com.example.android.waitlist.data.WaitlistDbHelper;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private GuestListAdapter mAdapter;
 
     // TODO (1) Create a local field member of type SQLiteDatabase called mDb
+    private SQLiteDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +37,19 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new GuestListAdapter(this);
 
         // TODO (2) Create a WaitlistDbHelper instance, pass "this" to the constructor as context
+        WaitlistDbHelper dbHelper = new WaitlistDbHelper(this);
 
         // TODO (3) Get a writable database reference using getWritableDatabase and store it in mDb
+        mDb = dbHelper.getWritableDatabase();
 
         // TODO (4) call insertFakeData from TestUtil and pass the database reference mDb
+        TestUtil.insertFakeData(mDb);
 
         // TODO (7) Run the getAllGuests function and store the result in a Cursor variable
+        Cursor cursor = getAllGuests();
 
         // TODO (12) Pass the resulting cursor count to the adapter
+        mAdapter = new GuestListAdapter(this, cursor.getCount());
 
         // Link the adapter to the RecyclerView
         waitlistRecyclerView.setAdapter(mAdapter);
@@ -54,6 +66,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // TODO (5) Create a private method called getAllGuests that returns a cursor
+    private Cursor getAllGuests() {
+        return mDb.query(WaitlistContract.WaitlistEntry.TABLE_NAME,
+                null, null, null, null, null, WaitlistContract.WaitlistEntry.COLUMN_TIMESTAMP);
+    }
 
     // TODO (6) Inside, call query on mDb passing in the table name and projection String [] order by COLUMN_TIMESTAMP
 

5cadd60020aeb0833bd264efebd8b318098b53cd
haozheng <hza89@sfu.ca>
2018-01-17 21:53:13 -0800

    a

--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -12,3 +12,24 @@ package com.example.android.background.sync;
 // TODO (3) Create a private static void method called incrementWaterCount
 // TODO (4) Add a Context called context to the argument list
 // TODO (5) From incrementWaterCount, call the PreferenceUtility method that will ultimately update the water count
+
+
+import android.content.Context;
+
+import com.example.android.background.utilities.PreferenceUtilities;
+
+public class ReminderTasks {
+    public static final String ACTION_INCREMENT_WATER_COUNT = "increment-water-count";
+
+    public static void executeTask(Context context, String action) {
+        if (ACTION_INCREMENT_WATER_COUNT.equals(action)) {
+            incrementWaterCount(context);
+
+        }
+    }
+
+    private static void incrementWaterCount(Context context) {
+        PreferenceUtilities.incrementWaterCount(context);
+
+    }
+}
--- Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
+++ Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
@@ -21,3 +21,23 @@
 //  TODO (11) Override onHandleIntent
 //      TODO (12) Get the action from the Intent that started this Service
 //      TODO (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+
+
+package com.example.android.background.sync;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+public class WaterReminderIntentService extends IntentService {
+
+    public WaterReminderIntentService() {
+        super("WaterReminderIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String action = intent.getAction();
+        ReminderTasks.executeTask(this, action);
+    }
+}

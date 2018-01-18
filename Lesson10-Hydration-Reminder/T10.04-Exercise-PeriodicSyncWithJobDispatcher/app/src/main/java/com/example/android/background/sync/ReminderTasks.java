f77f7612daeb010ad37638114c814ee43934e3bf
haozheng <hza89@sfu.ca>
2018-01-18 14:39:13 -0800

    jobservice complete

--- Lesson10-Hydration-Reminder/T10.04-Exercise-PeriodicSyncWithJobDispatcher/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ Lesson10-Hydration-Reminder/T10.04-Exercise-PeriodicSyncWithJobDispatcher/app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -24,12 +24,15 @@ public class ReminderTasks {
 
     public static final String ACTION_INCREMENT_WATER_COUNT = "increment-water-count";
     public static final String ACTION_DISMISS_NOTIFICATION = "dismiss-notification";
+    static final String ACTION_CHARGING_REMINDER = "charging-reminder";
 
     public static void executeTask(Context context, String action) {
         if (ACTION_INCREMENT_WATER_COUNT.equals(action)) {
             incrementWaterCount(context);
         } else if (ACTION_DISMISS_NOTIFICATION.equals(action)) {
             NotificationUtils.clearAllNotifications(context);
+        } else if (ACTION_CHARGING_REMINDER.equals(action)){
+            issueChargingReminder(context);
         }
     }
 
@@ -44,4 +47,11 @@ public class ReminderTasks {
     // increment the charging reminder count (hint: there is a method for this in PreferenceUtilities)
     // When finished, you should be able to call executeTask with the correct parameters to execute
     // this task. Don't forget to add the code to executeTask which actually calls your new task!
+
+    private static void issueChargingReminder(Context context) {
+        PreferenceUtilities.incrementWaterCount(context);
+        NotificationUtils.remindUserBecauseCharging(context);
+    }
+
+
 }
--- Lesson10-Hydration-Reminder/T10.04-Exercise-PeriodicSyncWithJobDispatcher/app/src/main/java/com/example/android/background/sync/WaterReminderFirebaseJobService.java
+++ Lesson10-Hydration-Reminder/T10.04-Exercise-PeriodicSyncWithJobDispatcher/app/src/main/java/com/example/android/background/sync/WaterReminderFirebaseJobService.java
@@ -15,7 +15,52 @@
  */
 package com.example.android.background.sync;
 
-public class WaterReminderFirebaseJobService {
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
+
+public class WaterReminderFirebaseJobService extends JobService {
+
+    private AsyncTask mBackgroundTask;
+
+
+// This is called by the Job Dispatcher to tell us we should start our job. Keep in mind this
+//method is run on the application's main thread, so we need to offload work to a background
+//thread.
+    @Override
+    public boolean onStartJob(final JobParameters jobParameters) {
+        mBackgroundTask = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                Context context = WaterReminderFirebaseJobService.this;
+                ReminderTasks.executeTask(context, ReminderTasks.ACTION_CHARGING_REMINDER);
+                return null;
+
+            }
+
+            @Override
+            protected void onPostExecute(Object o) {
+                jobFinished(jobParameters, false);
+
+            }
+        };
+
+        mBackgroundTask.execute();
+
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        if (mBackgroundTask != null) {
+            mBackgroundTask.cancel(true);
+
+        }
+        return true;
+    }
     // TODO (3) WaterReminderFirebaseJobService should extend from JobService
 
     // TODO (4) Override onStartJob
